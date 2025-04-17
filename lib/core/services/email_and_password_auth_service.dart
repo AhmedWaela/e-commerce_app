f3c8117665fb1540d54bfe3bbed6e4b138de08c7
ahmedwael <ahmedwael99104@gmail.com>
2025-04-17 16:21:39 +0200

    writes some docs

--- lib/core/services/email_and_password_auth_service.dart
+++ lib/core/services/email_and_password_auth_service.dart
@@ -2,5 +2,25 @@ import '../../Features/authentication/data/models/user_model/user_model.dart';
 
 abstract class EmailAuthService {
   const EmailAuthService();
+
+  /// Registers a new user with the provided [UserModel] details using the backend authentication service.
+  /// 
+  /// This method creates a user account with the given [user.email] and [user.password]. 
+  /// It delegates the actual account creation process to the underlying backend authentication service.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// final SupabaseEmailAuthService emailAuthService = 
+  ///     SupabaseEmailAuthService(Supabase.instance.auth);
+  ///
+  /// final UserModel user = UserModel(
+  ///   email: "ahmedwael99104@gmail.com",
+  ///   password: "123456789",
+  /// );
+  ///
+  /// await emailAuthService.createUserWithEmailAndPassword(user);
+  /// ```
+  ///
+  /// Throws an [Exception] if the registration fails or if invalid details are provided.
   Future<void> createUserWithEmailAndPassword(UserModel user);
 }
